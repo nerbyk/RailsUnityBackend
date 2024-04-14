3898eb042c466a2c75b6316492e94e7eb078f904
Nerbyk <danil.maximov2000@gmail.com>
2024-04-14 20:35:04 +0700

    Add sidekiq to dev docker env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
   config.on(:startup) do
     schedule_file = "config/schedule.yml"
 
